dcd4c831a6bf497721fbc12e636d2c8dc6c7fc79
Nawal003 <nawal.zouihig@gmail.com>
2021-01-12 07:23:59 +0100

    guessNb

--- GuessTheNumber/dist/guess.dev.js
+++ GuessTheNumber/dist/guess.dev.js
@@ -1,21 +1,61 @@
 "use strict";
 
+// let getRandomNumber = function (start, range) {
+//     let getRandom = Math.floor((Math.random() * range) + start);
+//     // si on veut que le nombre de dépasse pas un certain nombre il faut une boucle while
+//     while (getRandom > range) {
+//         getRandom = Math.floor((Math.random() * range) + start);
+//     }
+//     return getRandom;
+// }
+// console.log(getRandomNumber(0, 11));
 var output = document.querySelector('h2');
-var button = document.querySelector('button');
+var guess = document.getElementById('guess');
+var playAgain = document.getElementById('reset');
+var popUp = document.getElementById('pop-up');
+var yesBtn = document.getElementById('yes');
+var noBtn = document.getElementById('no'); // essai = 1;
+
 var number = [Math.floor(Math.random() * 10)];
-button.addEventListener('click', function () {
+console.log(number);
+guess.addEventListener('click', function guessNumber() {
   var input = document.querySelector('input').value;
 
   if (input == number) {
     output.textContent = "Bravo, tu as devin\xE9!!, c'\xE9tait le ".concat(number);
+    output.style.backgroundColor = 'green';
+    setTimeout(function () {
+      popUp.style.visibility = 'visible';
+    }, 1000);
   } else if (input < number) {
     output.textContent = 'Oulala! Trop bas, essaye encore!';
+    output.style.backgroundColor = 'red';
   }
 
   ;
 
   if (input > number) {
     output.textContent = 'Ouf! Trop élevé, essaye encore!';
+    output.style.backgroundColor = 'red';
   }
 });
-console.log(button.addEventListener);
+var essais = 0;
+
+function afficherEssai() {
+  var nbEssai = document.querySelector('h3');
+  essais += 1;
+  nbEssai.textContent = "le nombre d'essais est de : ".concat(essais);
+}
+
+;
+playAgain.addEventListener('click', function () {
+  location.reload();
+});
+yesBtn.addEventListener('click', function () {
+  popUp.style.visibility = 'hidden';
+  location.reload();
+});
+noBtn.addEventListener('click', function () {
+  popUp.style.visibility = 'hidden';
+  window.close();
+});
--- GuessTheNumber/guess.js
+++ GuessTheNumber/guess.js
@@ -1,21 +1,74 @@
-let output = document.querySelector('h2');
-let button = document.querySelector('button');
+// let getRandomNumber = function (start, range) {
+    //     let getRandom = Math.floor((Math.random() * range) + start);
+    //     // si on veut que le nombre de dépasse pas un certain nombre il faut une boucle while
+    
+    //     while (getRandom > range) {
+        //         getRandom = Math.floor((Math.random() * range) + start);
+        //     }
+        //     return getRandom;
+        // }
+        // console.log(getRandomNumber(0, 11));
+        
+        
+        
+        let output = document.querySelector('h2');
+        let guess = document.getElementById('guess');
+        let playAgain = document.getElementById('reset')
+        let popUp = document.getElementById('pop-up');
+        let yesBtn = document.getElementById('yes');
+        let noBtn = document.getElementById('no');
+
+// essai = 1;
+
 
 
 let number = [Math.floor(Math.random() * 10)];
+console.log(number);
 
-button.addEventListener('click', () => {
+guess.addEventListener('click', function guessNumber() {
     let input = document.querySelector('input').value;
     if (input == number){
         output.textContent = `Bravo, tu as deviné!!, c'était le ${number}`;
+        output.style.backgroundColor = 'green';
+        setTimeout(() => {
+            popUp.style.visibility = 'visible'
+        }, 1000);    
+       
+
     } else if (input < number){
         output.textContent = 'Oulala! Trop bas, essaye encore!';
-        
+        output.style.backgroundColor = 'red';
+
     };
     if (input > number) {
         output.textContent = 'Ouf! Trop élevé, essaye encore!';
+        output.style.backgroundColor = 'red';
+
 
     }
 });
 
-console.log(button.addEventListener);
+let essais = 0;
+
+function afficherEssai () {
+    let nbEssai = document.querySelector('h3');
+    essais += 1;   
+    nbEssai.textContent = `le nombre d'essais est de : ${essais}`;
+};
+
+playAgain.addEventListener('click',() => { 
+    location.reload();
+});
+
+
+
+
+yesBtn.addEventListener('click', () =>{
+    popUp.style.visibility = 'hidden';
+    location.reload();
+});
+
+noBtn.addEventListener('click', () => {
+    popUp.style.visibility = 'hidden';
+    window.close();
+});
